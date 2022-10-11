094e129b83a1a54f5345bcd0742f39575cc1d341
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2022-10-11 09:56:38 +0530

    feat: Add utility functions - `isIterable`, `isAsyncIterable`

--- src/core/utils/is.ts
+++ src/core/utils/is.ts
@@ -1,4 +1,6 @@
 export const isPromise = (promise: any) => typeof promise?.then === 'function'
-export const isObject = (item: any) => typeof item === "object" && !Array.isArray(item) && item !== null
+export const isObject = (value: unknown) => typeof value === "object" && !Array.isArray(value) && value !== null
 export const isAsyncFn = (fn: Function) => isFn(fn) && fn.constructor.name === 'AsyncFunction'
 export const isFn = (fn: Function) => typeof fn === 'function'
+export const isIterable = <T>(value?: unknown): value is (object & Iterable<T>) => typeof value === 'object' && value !== null && Symbol.iterator in value;
+export const isAsyncIterable = <T>(value?: unknown): value is (object & AsyncIterable<T>) => typeof value === 'object' && value !== null && Symbol.asyncIterator in value;
